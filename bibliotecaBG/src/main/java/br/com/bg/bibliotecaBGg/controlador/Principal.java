d13837b6e3cd3bd29c18304b5f8dc1ec64acf1ca
Lucas Leopoldino <loderdakar@gmail.com>
2018-09-20 20:37:32 -0300

    Criação dos pacotes e preparação da classe principal.

---
+++ bibliotecaBG/src/main/java/br/com/bg/bibliotecaBGg/controlador/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bg.bibliotecaBGg.controlador;
+
+import telas.TelaMenu;
+
+/**
+ *
+ * @author Loderdakar
+ */
+public class Principal {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        TelaMenu telaMenu = new TelaMenu();
+        telaMenu.setVisible(true);
+    }
+}
